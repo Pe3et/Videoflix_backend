5dd0b641ebfd2353dc8fdebc2c8d0c619c4d8067
Pe3et <wolfpeeet@googlemail.com>
2025-03-05 11:45:40 +0100

    wrote a test for correct registration

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class RegisterTests(APITestCase):
+    url = reverse('register')
+    
+    """
+    Tests the correct registration.
+    """
+    def test_correct_registration(self):
+        registration_data = {
+            'email': 'test@mail.com',
+            'password': 'password123',
+        }
+        response = self.client.post(self.url, registration_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['email'], registration_data['email'])
+
+    def test_wrong_registration(self):
+        # Test an invalid email field.
+        pass
+
+    def test_duplicate_registration(self):
+        # Test what happens, when user already exists.
+        pass
--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from auth_app.views import login_view, register_view
 
 urlpatterns = [
-    path('register/', register_view),
-    path('login/', login_view),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
 ]
