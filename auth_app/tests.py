aff7d6549ad9d112c56a2473cf5b8069ed83fecf
Pe3et <wolfpeeet@googlemail.com>
2025-03-06 15:21:21 +0100

    changed the email for the registration tests, since sending an email validation will be implemented

--- auth_app/tests.py
+++ auth_app/tests.py
@@ -10,7 +10,7 @@ class RegisterTests(APITestCase):
     """
     def test_correct_registration(self):
         registration_data = {
-            'email': 'test@mail.com',
+            'email': 'test@peeet.net',
             'password': 'password123',
         }
         response = self.client.post(self.url, registration_data, format='json')
@@ -22,7 +22,7 @@ class RegisterTests(APITestCase):
     """
     def test_wrong_registration(self):
         registration_data = {
-            'email': 'test@mail',
+            'email': 'test@',
             'password': 'password123',
         }
         response = self.client.post(self.url, registration_data, format='json')
@@ -34,7 +34,7 @@ class RegisterTests(APITestCase):
     """
     def test_missing_registration_data(self):
         registration_data = {
-            'email': 'test@mail'
+            'email': 'test@peeet.net'
         }
         response = self.client.post(self.url, registration_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -46,7 +46,7 @@ class RegisterTests(APITestCase):
     def test_duplicate_registration(self):
         self.test_correct_registration()
         registration_data = {
-            'email': 'test@mail.com',
+            'email': 'test@peeet.net',
             'password': 'password123',
         }
         response = self.client.post(self.url, registration_data, format='json')
