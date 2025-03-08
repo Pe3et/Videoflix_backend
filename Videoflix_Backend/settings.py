5dbc916a6279460a0a6491135c9054a859c68b2b
Pe3et <wolfpeeet@googlemail.com>
2025-03-08 07:17:30 +0100

    added corsheaders to installed apps

--- Videoflix_Backend/settings.py
+++ Videoflix_Backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,11 +57,13 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
  'http://127.0.0.1:5500',
  'http://localhost:5500',
+ 'http://localhost:4200',
 ]
 
 CORS_ALLOWED_ORIGINS = [
  'http://127.0.0.1:5500',
  'http://localhost:5500',
+ 'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'Videoflix_Backend.urls'
