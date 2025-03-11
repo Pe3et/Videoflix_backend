725b768eb64d0b5ecdc35c4ee7096bc816650cb8
Pe3et <wolfpeeet@googlemail.com>
2025-03-11 14:08:43 +0100

    moved email credentials to .env so no credentials get exposed - if someone clones this project, it is possible for the testing to enter ones own email credentials in the .env

--- Videoflix_Backend/settings.py
+++ Videoflix_Backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,8 +151,8 @@ REST_FRAMEWORK = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'w01fb2e4.kasserver.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noreply@videoflix.peeet.net'
-EMAIL_HOST_PASSWORD = 'accessonlyfromrestrictedips'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
