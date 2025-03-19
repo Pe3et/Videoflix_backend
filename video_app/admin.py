60e63107161488bd3133dff7a1d7d6707dfd47dd
Pe3et <wolfpeeet@googlemail.com>
2025-03-19 06:17:43 +0100

    defining readonly fields for admin interface

--- video_app/admin.py
+++ video_app/admin.py
@@ -12,4 +12,17 @@ class VideoAdmin(admin.ModelAdmin):
     """
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
-        convert_video.delay(obj.id)
+        if not change:
+            convert_video.delay(obj.id)
+
+    from django.contrib import admin
+
+    """
+    Defining what fields are readonly for admins and access to original_file & thumbnail is only
+    for creation, not for changing.
+    """
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ['created_at', 'processed_120p','processed_360p', 'processed_720p', 'processed_1080p'] 
+        if obj:  
+            return readonly_fields+['original_file', 'thumbnail']
+        return readonly_fields
